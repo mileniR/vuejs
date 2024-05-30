d7f931de5ce72451b6d052a9c9586bd837a4d2bd
301-14 <milreyesca@uch.pe>
2024-05-29 20:14:22 -0500

    commit

--- app/app.js
+++ app/app.js
@@ -6,21 +6,25 @@ var app = new Vue({
         discotecas: [
             { 
                 nombre: 'Di Luna',
+                descripcion: 'Ofrece buenos cócteles · Tiene música en vivo · Se va a bailar',
                 ubicacion: 'Multicenter, Carlos Izaguirre 275 2do. Nivel',
                 imagen: 'img/dis1.jpg'
             },
             { 
                 nombre: 'Asia Disco Club',
+                descripcion: 'Popular entre los jóvenes locales, con una pista de baile amplia y un área VIP exclusiva.',
                 ubicacion: 'C. C, Lima 15311',
                 imagen: 'img/dis2.jpg'
             },
             { 
                 nombre: 'Murano',
+                descripcion: 'Unica con una temática rústica y una amplia selección de cócteles artesanales.',
                 ubicacion: 'Av. Carlos Izaguirre 275',
                 imagen: 'img/dis3.jfif'
             },
             { 
                 nombre: 'Discoteca Paris',
+                descripcion: 'Puedes pasar momentos inolvidables en Paris degustando algunas de sus exquisiteces y bebidas',
                 ubicacion: 'Av. Uruguay 148, Lima 15001',
                 imagen: 'img/dis4.jpg'
             }
